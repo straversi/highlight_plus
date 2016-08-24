71406b78f4c8972638e1d2020514258b06d39e1e
Steven Traversi <straversi@berkeley.edu>
2016-08-23 20:03:16 -0700

    add filename option to highlight tag, add jekyll example project

--- jekyll_3.1.6/_plugins/highlight_plus.rb
+++ jekyll_3.1.6/_plugins/highlight_plus.rb
@@ -10,11 +10,25 @@ module Jekyll
       # <quoted list> is a space-separated list of numbers
       SYNTAX = %r!^([a-zA-Z0-9.+#-]+)((\s+\w+(=([a-zA-Z0-9.+'-_]+|"([0-9]+\s)*[0-9]+"))?)*)$!
 
+      def initialize(tag_name, markup, tokens)
+        super
+        if markup.strip =~ SYNTAX
+          @lang = Regexp.last_match(1).downcase
+          @highlight_options = parse_options(Regexp.last_match(2))
+        else
+          raise SyntaxError, <<-eos
+Syntax Error in tag 'highlight' while parsing the following markup:
+  #{markup}
+Valid syntax: highlight <lang> [linenos]
+eos
+        end
+      end
+
       def parse_options(input)
         options = {}
         unless input.empty?
           # Split along 3 possible forms -- key="<quoted list>", key=value, or key
-          input.scan(%r!(?:\w="[^"]*"|\w=[a-zA-Z0-9.+'-_]*|\w)+!) do |opt|
+          input.scan(%r!(?:\w="[^"]*"|\w=[a-zA-Z0-9.+'-_]+|\w)+!) do |opt|
             key, value = opt.split("=")
             # If a quoted list, convert to array
             if value && value.include?("\"")
